46fbf3451431adb877990748d4857ace770cafd4
jingqiancheng <jingqiancheng@brandeis.edu>
2022-02-17 23:31:28 -0500

    Jingqian's recording

--- course_search.py
+++ course_search.py
@@ -34,7 +34,7 @@ def topmenu():
     topmenu is the top level loop of the course search app
     '''
     global schedule
-    while True:        
+    while True:
         command = input(">> (h for help) ")
         if command=='quit':
             return
@@ -52,7 +52,7 @@ def topmenu():
         elif command in ['s','subject']:
             subject = input("enter a subject:")
             schedule = schedule.subject([subject])
-        #7.a --Jingqian 
+        #7.a --Jingqian
         elif command in ['c','course']:
             coursenum = input("enter a coursenum:")
             schedule = schedule.coursenum(coursenum)
